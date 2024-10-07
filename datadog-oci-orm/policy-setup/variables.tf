3bda8b6805fe88b4d90cac5431a86b222feae840
Kanishk Tripathi <kanishk.tripathi@datadoghq.com>
2024-10-07 11:55:46 -0400

    ECI-258 Create a terraform stack that adds an auth user, group and policy

--- datadog-oci-orm/policy-setup/variables.tf
+++ datadog-oci-orm/policy-setup/variables.tf
@@ -16,9 +16,22 @@ variable "dynamic_group_name" {
   default     = "datadog-metrics-dynamic-group"
 }
 
+variable "user_group_name" {
+  type        = string
+  description = "The name of the group for giving access to user"
+  default     = "DatadogAuthGroup"
+}
+
 variable "datadog_metrics_policy" {
   type        = string
   description = "The name of the policy for metrics"
   default     = "datadog-metrics-policy"
 }
+
+variable "user_domain" {
+  type        = string
+  description = "The name of the domain where the operation is to be run. It is assumed that the setup will create users and groups in the default domain. Enter the domain name if the user running the setup does not belong to Default domain."
+  default     = "Default"
+}
+
 
