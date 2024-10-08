029b26292e6f2d6ce07b13527e777d0703458190
Shallav Varma <shallav.varma@datadoghq.com>
2024-10-08 12:50:39 -0400

    Add unit tests

--- Service Connector  Hub/tests/test_func.py
+++ Service Connector  Hub/tests/test_func.py
@@ -6,7 +6,7 @@ from io import BytesIO
 # Add the directory containing func.py to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
-from func import handler
+from func import handler, DD_BATCH_SIZE
 from unittest import TestCase, mock
 
 
@@ -14,12 +14,15 @@ def to_bytes_io(inp : str):
     """ Helper function to turn string test data into expected BytesIO asci encoded bytes """
     return BytesIO(bytes(inp, 'ascii'))
 
+
 def _decompress_string(compressed_string):
     """Decompresses a gzip-compressed string."""
     return gzip.decompress(compressed_string).decode()
 
-def to_decompressed_dict(data) -> dict :
+
+def to_decompressed_data(data) -> list[dict] :
     return eval(_decompress_string(data))
+
 
 class TestLogForwarderFunction(TestCase):
     """ Test simple and batch format json CloudEvent payloads """
@@ -46,18 +49,20 @@ class TestLogForwarderFunction(TestCase):
         }
     """
 
-    SAMPLE_OUTPUT = {
-        "source": "Log_Connector",
-        "timestamp": "2024-09-25T20:04:45.130Z",
-        "data":
+    SAMPLE_OUTPUT = [
         {
-            "level": "INFO",
-            "message": "Run succeeded - Read 0 messages from source and wrote 0 messages to target",
-            "messageType": "CONNECTOR_RUN_COMPLETED"
-        },
-        "ddsource": "oracle_cloud",
-        "service": "OCI Logs"
-    }
+            "source": "Log_Connector",
+            "timestamp": "2024-09-25T20:04:45.130Z",
+            "data":
+            {
+                "level": "INFO",
+                "message": "Run succeeded - Read 0 messages from source and wrote 0 messages to target",
+                "messageType": "CONNECTOR_RUN_COMPLETED"
+            },
+            "ddsource": "oracle_cloud",
+            "service": "OCI Logs"
+        }
+    ]
 
 
     def setUp(self):
@@ -72,11 +77,12 @@ class TestLogForwarderFunction(TestCase):
         """ Test single CloudEvent payload """
 
         payload = TestLogForwarderFunction.SAMPLE_INPUT
+        mock_post.reset_mock()
         handler(ctx=None, data=to_bytes_io(payload))
         mock_post.assert_called_once()
-        self.assertDictEqual(
+        self.assertEqual(
             TestLogForwarderFunction.SAMPLE_OUTPUT,
-            to_decompressed_dict(mock_post.mock_calls[0].kwargs['data'])
+            to_decompressed_data(mock_post.mock_calls[0].kwargs['data'])
         )
         self.assertEqual(
             "gzip",
@@ -91,28 +97,39 @@ class TestLogForwarderFunction(TestCase):
         payload = TestLogForwarderFunction.SAMPLE_INPUT
         os.environ['DATADOG_TAGS'] = "prod:true"
         handler(ctx=None, data=to_bytes_io(payload))
-        expected_output = dict(TestLogForwarderFunction.SAMPLE_OUTPUT)
+        expected_output = dict(TestLogForwarderFunction.SAMPLE_OUTPUT[0])
         expected_output['ddtags'] = os.environ['DATADOG_TAGS']
+        expected_output = [expected_output]
         mock_post.assert_called_once()
-        self.assertDictEqual(
+        self.assertEqual(
             expected_output,
-            to_decompressed_dict(mock_post.mock_calls[0].kwargs['data'])
+            to_decompressed_data(mock_post.mock_calls[0].kwargs['data'])
         )
 
 
     @mock.patch("requests.post")
     def test_batch_format(self, mock_post):
-        """ Test batch format case, where we get an array of 'CloudEvents' """
-        batch_count = 10
-        payload = f"""
-        [
-            {",".join([TestLogForwarderFunction.SAMPLE_INPUT] * batch_count)}
-        ]
-        """
-        expected_output = [dict(TestLogForwarderFunction.SAMPLE_OUTPUT)] * batch_count
-        handler(ctx=None, data=to_bytes_io(payload))
-        self.assertEqual(batch_count, mock_post.call_count, "Data was successfully submitted for the entire batch")
-        self.assertEqual(
-            expected_output,
-            [to_decompressed_dict(arg.kwargs['data']) for arg in mock_post.call_args_list]
-        )
+        """Test batch format case, where we get an array of 'CloudEvents'."""
+        batch_counts = [10, 999, 1000, 1001, 10000]
+        for batch_count in batch_counts:
+            with self.subTest(msg=f"Batch Count: {batch_count}"):
+                mock_post.reset_mock()  # Reset mock_post before each subtest
+                payload = f"""
+                [
+                    {",".join([TestLogForwarderFunction.SAMPLE_INPUT] * batch_count)}
+                ]
+                """
+                total_calls = (batch_count + DD_BATCH_SIZE - 1) // DD_BATCH_SIZE
+                handler(ctx=None, data=to_bytes_io(payload))
+                self.assertEqual(
+                    total_calls, 
+                    mock_post.call_count, 
+                    "Data was successfully submitted for the entire batch"
+                )
+                for i in range(total_calls):
+                    call_count = DD_BATCH_SIZE if (i < total_calls - 1 or batch_count % DD_BATCH_SIZE == 0) else batch_count % DD_BATCH_SIZE
+                    expected_output = [dict(TestLogForwarderFunction.SAMPLE_OUTPUT[0])] * call_count
+                    self.assertEqual(
+                        expected_output,
+                        to_decompressed_data(mock_post.mock_calls[i].kwargs['data'])
+                    )
