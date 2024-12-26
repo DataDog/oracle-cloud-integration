807c1919867b2d62456aef18ac3e8691ffcf88fa
Shallav <shallav.varma@datadoghq.com>
2024-12-26 15:52:30 -0500

    Update datadog-logs-oci-orm/modules/containerregistry/locals.tf
    Co-authored-by: Ben Gillman <165046932+bengillmandd@users.noreply.github.com>

--- datadog-logs-oci-orm/modules/containerregistry/locals.tf
+++ datadog-logs-oci-orm/modules/containerregistry/locals.tf
@@ -2,8 +2,8 @@ locals {
   # OCI docker repository
   registry_domain = "ocir.${var.region}.oci.oraclecloud.com"
   tenancy_namespace = data.oci_objectstorage_namespace.namespace.namespace
-  function_name         = "datadog-function-logs"
-  repo_name        = "${var.resource_name_prefix}-functions/${local.function_name}"
-  docker_image_path     = "${local.registry_domain}/${local.tenancy_namespace}/${local.repo_name}"
+  function_name = "datadog-function-logs"
+  repo_name = "${var.resource_name_prefix}-functions/${local.function_name}"
+  docker_image_path = "${local.registry_domain}/${local.tenancy_namespace}/${local.repo_name}"
   username = data.oci_identity_user.docker_user.name
 }
