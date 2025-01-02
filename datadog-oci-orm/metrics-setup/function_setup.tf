72e6ae606b6adddd5e6ca65d3f7f359fe8541b2e
Isabella Rhee <bella.rhee@datadoghq.com>
2025-01-02 16:39:23 -0500

    add resiliency to metric stack setup

--- datadog-oci-orm/metrics-setup/function_setup.tf
+++ datadog-oci-orm/metrics-setup/function_setup.tf
@@ -1,10 +1,36 @@
 resource "null_resource" "Login2OCIR" {
-  provisioner "local-exec" {
-    command = "echo '${var.oci_docker_password}' |  docker login ${local.oci_docker_repository} --username ${local.ocir_namespace}/${var.oci_docker_username} --password-stdin"
-  }
+   provisioner "local-exec" {
+       command = <<EOT
+           #!/bin/bash
+           set -e
+
+           for i in {1..5}; do
+               echo "${var.oci_docker_password}" | docker login ${local.oci_docker_repository} --username ${local.ocir_namespace}/${var.oci_docker_username} --password-stdin && break
+               echo "Retrying Docker login... attempt $i"
+               sleep 10
+           done
+
+           # Check if login was successful
+           if [ $i -eq 5 ]; then
+               echo "Error: Docker login failed after 5 attempts. Trying to login through Oracle Identity Cloud Service"
+               for j in {1..5}; do
+                   echo "${var.oci_docker_password}" | docker login ${local.oci_docker_repository} --username ${local.ocir_namespace}/oracleidentitycloudservice/$username --password-stdin && break
+                   echo "Retrying Docker login through Identity service... attempt $j"
+                   sleep 5
+               done
+               if [ $j -eq 5 ]; then
+                   echo "Error: Docker login failed after 5 attempts."
+                   exit 1
+               fi
+           fi
+       EOT
+   }
+   triggers = {
+       always_run = "${timestamp()}"
+   }
 }
 
-### Repository in the Container Image Registry for the container images underpinning the function 
+### Repository in the Container Image Registry for the container images underpinning the function
 resource "oci_artifacts_container_repository" "function_repo" {
   # note: repository = store for all images versions of a specific container image - so it included the function name
   depends_on     = [null_resource.Login2OCIR]
@@ -18,10 +44,6 @@ resource "oci_artifacts_container_repository" "function_repo" {
 # ### build the function into a container image and push that image to the repository in the OCI Container Image Registry
 resource "null_resource" "FnImagePushToOCIR" {
   depends_on = [oci_artifacts_container_repository.function_repo, oci_functions_application.metrics_function_app, null_resource.Login2OCIR]
-
-  provisioner "local-exec" {
-    command = "echo '${var.oci_docker_password}' |  docker login ${local.oci_docker_repository} --username ${local.ocir_namespace}/${var.oci_docker_username} --password-stdin"
-  }
 
   # remove function image (if it exists) from local container registry
   provisioner "local-exec" {
@@ -47,4 +69,11 @@ resource "null_resource" "FnImagePushToOCIR" {
     working_dir = "metrics-function"
   }
 
+}
+
+resource "null_resource" "wait_for_image" {
+  depends_on = [null_resource.FnImagePushToOCIR]
+  provisioner "local-exec" {
+    command = "sleep 60"
+  }
 }
