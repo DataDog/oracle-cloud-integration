0182be1b689b773936b8a93e5759888d1aff98bf
Shallav Varma <shallav.varma@datadoghq.com>
2025-02-25 16:43:05 -0500

    add cache to prevent regenerating client if not needed

--- metrics-forwarder/internal/client/client.go
+++ metrics-forwarder/internal/client/client.go
@@ -8,6 +8,8 @@ import (
 	datadog "github.com/DataDog/datadog-api-client-go/v2/api/datadog"
 )
 
+var cache *DatadogClient
+
 type apiClient interface {
 	CallAPI(req *http.Request) (*http.Response, error)
 	PrepareRequest(ctx context.Context, path string, method string, postBody interface{}, headerParams map[string]string, queryParams url.Values, formParams url.Values, fileName *datadog.FormFile) (*http.Request, error)
@@ -20,13 +22,19 @@ type DatadogClient struct {
 }
 
 func NewDatadogClient(site, apiKey string) DatadogClient {
+	if cache != nil && cache.site == site && cache.apiKey == apiKey {
+		return *cache
+	}
+
 	configuration := datadog.NewConfiguration()
 	configuration.RetryConfiguration.EnableRetry = true
 	client := datadog.NewAPIClient(configuration)
 
-	return DatadogClient{
+	cache = &DatadogClient{
 		Client: client,
 		site:   site,
 		apiKey: apiKey,
 	}
+
+	return *cache
 }
