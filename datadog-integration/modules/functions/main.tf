5d94011cae4b19d6ed7c7ecc42e22bc8362a1a95
Shallav Varma <shallav.varma@datadoghq.com>
2025-02-27 16:30:06 -0500

    Bump memory for functions

--- datadog-integration/modules/functions/main.tf
+++ datadog-integration/modules/functions/main.tf
@@ -13,7 +13,7 @@ resource "oci_functions_function" "logs_function" {
   count           = var.logs_image_tag != "" ? 1 : 0
   application_id  = oci_functions_application.dd_function_app.id
   display_name    = "dd-logs-forwarder"
-  memory_in_mbs   = "256"
+  memory_in_mbs   = "1024"
   freeform_tags   = var.tags
   image           = local.logs_image_path
 }
@@ -22,7 +22,7 @@ resource "oci_functions_function" "metrics_function" {
   count           = var.metrics_image_tag != "" ? 1 : 0
   application_id  = oci_functions_application.dd_function_app.id
   display_name    = "dd-metrics-forwarder"
-  memory_in_mbs   = "256"
+  memory_in_mbs   = "512"
   freeform_tags   = var.tags
   image           = local.metrics_image_path
 }
