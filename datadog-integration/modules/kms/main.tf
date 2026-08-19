764d2d6d27e4a598fa5c188ae147a6b1f0c85bbb
johann.zhang <johann.zhang@datadoghq.com>
2026-08-19 17:21:42 -0400

    upgrade oci version to 8.5

--- datadog-integration/modules/kms/main.tf
+++ datadog-integration/modules/kms/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     oci = {
       source  = "oracle/oci"
-      version = ">=7.1.0"
+      version = ">= 8.5.0, < 9.0.0"
     }
     null = {
       source  = "hashicorp/null"
