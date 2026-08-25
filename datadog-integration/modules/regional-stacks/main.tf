fd2c00a3aa5fac2a6fa3c9e37f284926cd320cc7
johann.zhang <johann.zhang@datadoghq.com>
2026-08-25 16:45:05 -0400

    pin 8.1, because that seems to work too

--- datadog-integration/modules/regional-stacks/main.tf
+++ datadog-integration/modules/regional-stacks/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     oci = {
       source  = "oracle/oci"
-      version = "~> 8.5"
+      version = "~> 8.1"
     }
     http = {
       source  = "hashicorp/http"
